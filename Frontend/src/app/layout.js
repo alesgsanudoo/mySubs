c438f536b7b8e36b3e6c36aee2b3007a8b3d74cd
Alejandro S <alejandrosanudo10@gmail.com>
2024-11-06 21:45:21 -0500

    Added favicon and proper name

--- Frontend/src/app/layout.js
+++ Frontend/src/app/layout.js
@@ -7,14 +7,24 @@ import CookieBanner from '@/components/CookieBanner';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "MySubs Tracker",
-  description: "Track all your subscriptions",
+    title: "My Subs ⋅ Track your subscriptions",
+    description: "Personal Project of Alejandro Griffith",
+    icons: {
+        icon: [
+            {rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png"},
+            {rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon-32x32.png"},
+            {rel: "icon", type: "image/png", sizes: "16x16", url: "/favicon-16x16.png"},
+            {rel: "shortcut icon", url: "/favicon.ico"},
+        ],
+        manifest: "/site.webmanifest",
+        maskIcon: {rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#5bbad5"},
+    },
 };
 
 export default function RootLayout({ children }) {
   return (
       <html lang="en">
-      <body>{children}<Toaster /><CookieBanner /></body>
+      <body>{children}<Toaster /></body>
       </html>
   )
 }
